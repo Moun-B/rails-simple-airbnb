14eb65087f8f2b70a5593b1e5850559eb50f4ad8
Mounir EL JAMGHILI <eljmounir@gmail.com>
2022-08-10 18:31:50 +0900

    finished

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -7,6 +7,15 @@ class FlatsController < ApplicationController
     @flat = Flat.new
   end
 
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new, status: :unauthorized
+    end
+  end
+
   def show
     @flat = Flat.find(params[:id])
   end
