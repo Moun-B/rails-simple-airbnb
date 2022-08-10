1fcdabd77e07a9a2ae18072c46c918d8532fbdb8
Mounir EL JAMGHILI <eljmounir@gmail.com>
2022-08-10 16:45:19 +0900

    finished everything except search

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Destroying all the filthies flats"
+Flat.destroy_all
+puts "Creating new crazy flats"
+4.times do
+  Flat.create!(
+    name: Faker::Address.city,
+    address: Faker::Address.full_address,
+    description: Faker::Food.description,
+    price_per_night: rand(10..100),
+    number_of_guests: rand(1..8),
+  )
+end
+
+puts "Finished creating #{Flat.count} flats!"
